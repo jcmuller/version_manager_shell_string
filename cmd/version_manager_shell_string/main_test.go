d4732de5de7a4998428a71621809cad8ac9cc8e4
Juan C. Müller <jcmuller@gmail.com>
2018-08-24 18:26:59 -0400

    Simpler test cases

--- cmd/version_manager_shell_string/main_test.go
+++ cmd/version_manager_shell_string/main_test.go
@@ -1,49 +1,50 @@
 package main
 
 import (
-	"testing"
+	"fmt"
 	"os"
+	"strings"
+	"testing"
 )
 
+func TestFindDirectoryNoDirectory(t *testing.T) {
+	_, err := findDirectory("foo")
+
+	if err == nil {
+		t.Error("findDirectory(foo) is supposed to error out!")
+	}
+}
+
+func setupProject() (base, dir string) {
+	tmp := os.TempDir()
+	base = strings.Join([]string{tmp, "go", "vmss", "test"}, "/")
+	dir = fmt.Sprintf("%s/foo/bar", base)
+	gitDir := fmt.Sprintf("%s/.git", base)
+
+	os.MkdirAll(gitDir, 0755)
+	os.MkdirAll(dir, 0755)
+
+	return
+}
+
+func cleanUpProject(base string) {
+	os.RemoveAll(base)
+}
+
 func TestFindDirectory(t *testing.T) {
-	os.MkdirAll("/tmp/go/test/.git", 0755)
-	os.MkdirAll("/tmp/go/test/foo/bar", 0755)
+	base, dir := setupProject()
 
-	type args struct {
-		input string
-	}
-	tests := []struct {
-		name     string
-		args     args
-		expected string
-		wantErr  bool
-	}{
-		{
-			name:     "Empty directory",
-			args:     args{"foo"},
-			expected: "",
-			wantErr:  true,
-		},
-		{
-			name:     "Good directory",
-			args:     args{"/tmp/go/test/foo/bar"},
-			expected: "/tmp/go/test",
-			wantErr:  false,
-		},
+	actual, err := findDirectory(dir)
+
+	if err != nil {
+		t.Errorf("findDirectory() is not supposed to err: %v", err)
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			actual, err := findDirectory(tt.args.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("findDirectory() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if actual != tt.expected {
-				t.Errorf("findDirectory() = %v, expected %v", actual, tt.expected)
-			}
-		})
+	expected := base
+
+	if actual != base {
+		t.Errorf("findDirectory() error: expected: %v, actual: %v", expected, actual)
 	}
 
-	os.RemoveAll("/tmp/go/test")
+	cleanUpProject(base)
 }
